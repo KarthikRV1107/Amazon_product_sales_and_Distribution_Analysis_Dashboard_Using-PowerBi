3b0b55bbbd7c4213624738e43f03d8c7f6be9bc1
KarthikRV1107 <karthikrv1107@gmail.com>
2024-12-29 19:57:48 +0530

    code

---
+++ ex1/src/ex1/Condation.java
@@ -0,0 +1,24 @@
+package ex1;
+import java.util.Scanner;
+public class Condation {
+
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+	    int a = s.nextInt();
+	    int  b = s.nextInt();
+	     System.out.println("the addition of two numbers is: "+(a+b));
+	     System.out.println("the subraction of two numbers is: "+(a-b));
+	     System.out.println("the multipication of two numbers is: "+(a*b));
+	     System.out.println("the division of two numbers is: "+(a/b));
+	     
+		/*System.out.println("100"+a+"abc");
+		System.out.println(a+"Karthik"+a+"100");
+		System.out.println(a+b+"Karthik"+a+"100"+(a+b));
+		System.out.println("abc"+a+b+"Bro");
+		System.out.println(a+b+"Bro");
+		System.out.println("Bro"+a+"-"+b+"SOD");*/
+	}
+
+}
---
+++ ex1/src/ex1/Hello.java
@@ -0,0 +1,10 @@
+package ex1;
+
+ class Hello 
+ {
+	public static void main(String arg[])
+	{
+		System.out.println("Helloworld");
+	}
+	
+}
